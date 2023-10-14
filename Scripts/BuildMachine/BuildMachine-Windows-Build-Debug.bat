20b340d0e01ed7494e07466dbc9ea2d0e5b6028e
TrashCoder94 <jmottershead1994@gmail.com>
2023-10-14 13:23:27 +0100

    Update: Nuget Package Restore
    :window: - Attempting to restore any nuget packages for the solution before building tools. Just to make sure any window dependencies are ready!

--- Scripts/BuildMachine/BuildMachine-Windows-Build-Debug.bat
+++ Scripts/BuildMachine/BuildMachine-Windows-Build-Debug.bat
@@ -6,6 +6,9 @@ call ThirdParty\premake\Binaries\premake5.exe vs2022
 devenv GBTools.sln /rebuild Debug /project Premake
 if %errorlevel% neq 0 exit /b %errorlevel%
 
+echo "Restoring nuget packages"
+nuget restore GBTools.sln
+
 echo "Building Debug Configuration"
 devenv GBTools.sln /rebuild Debug
 if %errorlevel% neq 0 exit /b %errorlevel%
