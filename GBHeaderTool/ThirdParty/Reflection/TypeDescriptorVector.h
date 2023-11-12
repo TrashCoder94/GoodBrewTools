7e91e9b917fe00c539712ce5f4151797e2a8e532
TrashCoder94 <jmottershead1994@gmail.com>
2023-11-12 13:30:52 +0000

    Update: Header Tool
    Removing vector<bool> specialization since this is causing compile issues. More complicated than I initially thought and I don't quite understand what's happening with it, but I just know I can't implement a simple bool -> void pointer conversion for vectors. Won't support that data type (and probably don't even need to, would be nice to have though - maybe I will spend time in the future trying to figure it out).

--- GBHeaderTool/ThirdParty/Reflection/TypeDescriptorVector.h
+++ GBHeaderTool/ThirdParty/Reflection/TypeDescriptorVector.h
@@ -66,77 +66,12 @@ namespace reflect
 		}
 	};
 
-	struct TypeDescriptor_StdVector_Bool : TypeDescriptor {
-		TypeDescriptor* itemType;
-		void(*addNewItem)(const void*);
-		void(*removeItem)(const void*, size_t);
-		void(*clearAllItems)(const void*);
-		size_t(*getSize)(const void*);
-		const void* (*getItem)(const void*, size_t);
-
-		template <typename ItemType>
-		TypeDescriptor_StdVector_Bool(ItemType*, FieldType vectorFieldType = FieldType::Vector)
-			: TypeDescriptor{ "std::vector<>", sizeof(std::vector<ItemType>), vectorFieldType },
-			itemType{ TypeResolver<ItemType>::get() } {
-			addNewItem = [](const void* vecPtr) {
-				auto& vec = *(std::vector<ItemType>*) vecPtr;
-				vec.emplace_back();
-				};
-			removeItem = [](const void* vecPtr, size_t index) {
-				auto& vec = *(std::vector<ItemType>*) vecPtr;
-				vec.erase(vec.begin() + index);
-				};
-			clearAllItems = [](const void* vecPtr) {
-				auto& vec = *(std::vector<ItemType>*) vecPtr;
-				vec.clear();
-				};
-			getSize = [](const void* vecPtr) -> size_t {
-				const auto& vec = *(const std::vector<ItemType>*) vecPtr;
-				return vec.size();
-				};
-			getItem = [](const void* vecPtr, size_t index) -> const void* {
-				const auto& vec = *(const std::vector<ItemType>*) vecPtr;
-				int* pBoolAsIntValue = reinterpret_cast<int*>(vec[index]);
-				void* pIntAsVoidValue = reinterpret_cast<void*>(pBoolAsIntValue);
-				return pIntAsVoidValue;
-				};
-		}
-		virtual std::string getFullName() const override {
-			return std::string("std::vector<") + itemType->getFullName() + ">";
-		}
-		virtual void dump(const void* obj, int indentLevel) const override {
-			size_t numItems = getSize(obj);
-			std::cout << getFullName();
-			if (numItems == 0) {
-				std::cout << "{}";
-			}
-			else {
-				std::cout << "{" << std::endl;
-				for (size_t index = 0; index < numItems; index++) {
-					std::cout << std::string(4 * (indentLevel + 1), ' ') << "[" << index << "] ";
-					itemType->dump(getItem(obj, index), indentLevel + 1);
-					std::cout << std::endl;
-				}
-				std::cout << std::string(4 * indentLevel, ' ') << "}";
-			}
-		}
-	};
-
 	// Partially specialize TypeResolver<> for std::vectors:
 	template <typename T>
 	class TypeResolver<std::vector<T>> {
 	public:
 		static TypeDescriptor* get() {
 			static TypeDescriptor_StdVector typeDesc{ (T*) nullptr };
-			return &typeDesc;
-		}
-	};
-
-	template <>
-	class TypeResolver<std::vector<bool>> {
-	public:
-		static TypeDescriptor* get() {
-			static TypeDescriptor_StdVector_Bool typeDesc{ (bool*) nullptr };
 			return &typeDesc;
 		}
 	};
